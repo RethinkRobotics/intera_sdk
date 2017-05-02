140872ccff30b7431aa1ec628c7d5e8afe8477cd
Ian McMahon <imcmahon@rethinkrobotics.com>
2017-05-02 12:49:28 -0400

    Bumped SDK version to 5.2

--- intera_interface/src/intera_interface/settings.py
+++ intera_interface/src/intera_interface/settings.py
@@ -16,10 +16,12 @@ JOINT_ANGLE_TOLERANCE = 0.008726646
 HEAD_PAN_ANGLE_TOLERANCE = 0.1
 
 ## Versioning
-SDK_VERSION = '5.1.0'
+SDK_VERSION = '5.2.0'
 CHECK_VERSION = True
 # Version Compatibility Maps - {current: compatible}
-VERSIONS_SDK2ROBOT = {'5.1.0': ['5.1.0', '5.1.1', '5.1.2', '5.2.0']}
+VERSIONS_SDK2ROBOT = {'5.1.0': ['5.1.0', '5.1.1', '5.1.2'],
+                      '5.2.0': ['5.2.0']
+                     }
 VERSIONS_SDK2GRIPPER = {'5.1.0':
                           {
                            'warn': '2014/5/20 00:00:00',  # Version 1.0.0
