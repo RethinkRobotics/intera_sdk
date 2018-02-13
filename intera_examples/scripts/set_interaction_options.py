b067d8527d7a2c2d9c7c04ed3e74e4262a299ed2
Forrest Rogers-Marcovitz <frogers-marcovitz@rethinkrobotics.com>
2018-02-13 12:01:26 -0500

    When the robot is disabled, put the robot into position mode

--- intera_examples/scripts/set_interaction_options.py
+++ intera_examples/scripts/set_interaction_options.py
@@ -21,6 +21,9 @@ from geometry_msgs.msg import Pose
 from intera_motion_interface import InteractionOptions
 from intera_motion_interface.utility_functions import int2bool
 
+import intera_interface
+from intera_interface import CHECK_VERSION
+
 def main():
     """
     Set the desired interaction control options in the current configuration.
@@ -170,15 +173,21 @@ def main():
         rospy.loginfo(msg)
         pub.publish(msg)
 
+        rs = intera_interface.RobotEnable(CHECK_VERSION)
         if args.rate > 0:
-            while not rospy.is_shutdown():
+            while not rospy.is_shutdown() and rs.state().enabled:
                 rate.sleep()
                 pub.publish(msg)
-
     except rospy.ROSInterruptException:
         rospy.logerr('Keyboard interrupt detected from the user. %s',
                      'Exiting the node...')
 
+    if not rs.state().enabled:
+        # send a message to put the robot back into position mode
+        position_mode = InteractionOptions()
+        position_mode.set_interaction_control_active(False)
+        pub.publish(position_mode.to_msg())
+        rospy.sleep(0.5)
 
 if __name__ == '__main__':
     main()
