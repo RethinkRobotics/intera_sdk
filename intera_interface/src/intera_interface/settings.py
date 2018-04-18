a28d9d77a704cd93b51b070450a9d66c738e2837
Ian McMahon <ian@ianthe.engineer>
2018-04-18 16:15:39 -0400

    Updated settings.py for 5.3.0

--- intera_interface/src/intera_interface/settings.py
+++ intera_interface/src/intera_interface/settings.py
@@ -16,13 +16,14 @@ JOINT_ANGLE_TOLERANCE = 0.008726646
 HEAD_PAN_ANGLE_TOLERANCE = 0.1
 
 ## Versioning
-SDK_VERSION = '5.2.0'
+SDK_VERSION = '5.3.0'
 CHECK_VERSION = True
 # Version Compatibility Maps - {current: compatible}
 VERSIONS_SDK2ROBOT = {'5.1.0': ['5.1.0', '5.1.1', '5.1.2'],
-                      '5.2.0': ['5.2.0', '5.2.1', '5.2.2']
+                      '5.2.0': ['5.2.0', '5.2.1', '5.2.2'],
+                      '5.3.0': ['5.3.0']
                      }
-VERSIONS_SDK2GRIPPER = {'5.2.0':
+VERSIONS_SDK2GRIPPER = {'5.3.0':
                           {
                            'warn': '2014/5/20 00:00:00',  # Version 1.0.0
                            'fail': '2013/10/15 00:00:00', # Version 0.6.2
