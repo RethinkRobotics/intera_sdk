b140ba9d1b33e9864cf64b08a80e56868250a2fd
Ian McMahon <ian@ianthe.engineer>
2018-06-08 09:56:30 -0400

    Updated settings.py for 5.3.1

--- intera_interface/src/intera_interface/settings.py
+++ intera_interface/src/intera_interface/settings.py
@@ -21,7 +21,7 @@ CHECK_VERSION = True
 # Version Compatibility Maps - {current: compatible}
 VERSIONS_SDK2ROBOT = {'5.1.0': ['5.1.0', '5.1.1', '5.1.2'],
                       '5.2.0': ['5.2.0', '5.2.1', '5.2.2'],
-                      '5.3.0': ['5.3.0']
+                      '5.3.0': ['5.3.0', '5.3.1', '5.3.2']
                      }
 VERSIONS_SDK2GRIPPER = {'5.3.0':
                           {
